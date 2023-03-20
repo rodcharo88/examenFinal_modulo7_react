14971919768c8ac79f4852248750b7f8d60ecab0
Rodrigo Rocha <rodcharo@gmail.com>
2023-03-19 23:39:30 -0400

    Some fixes :sparkles:

--- src/stories/TableComp.jsx
+++ src/stories/TableComp.jsx
@@ -1,14 +1,6 @@
-import { useEffect } from "react";
+
 import './tableComp.css';
-// import PropTypes from 'prop-types'
-// import { TableProps } from './intefaces/TableProps.interface'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+
 
 const TableComp = ({filas, columnas, headers}) => {
     
@@ -28,7 +20,7 @@ const TableComp = ({filas, columnas, headers}) => {
       return (
         <table>
             <tr>{headers.map(headers => <th>{headers}</th>)}</tr>
-            {filasData.map(val => <tr>{columnasData.map(col => <th> {col} </th>)}</tr>)}
+            {filasData.map(val => <tr>{columnasData.map(col => <td> {col} </td>)}</tr>)}
         </table>
       )
 };
